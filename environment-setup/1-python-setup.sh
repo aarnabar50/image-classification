f0563eb0e63b2b2ae4471745da37db2023b245e0
aarna.bar <aarnabar50@gmail.com>
2025-11-09 11:28:47 -0500

    Add Python setup script for Homebrew installation and version checks

---
+++ environment-setup/1-python-setup.sh
@@ -0,0 +1,12 @@
+#Update homebrew
+brew update
+
+brew install python
+
+python3 --version
+
+brew reinstall python
+
+#At this point pip3 should be installed
+#check pip3 version
+pip3 --version
---
+++ environment-setup/2-git-setup..sh
@@ -0,0 +1,2 @@
+git config --global user.name "aarna.bar"
+git config --global user.email "aarnabar50@gmail.com"
